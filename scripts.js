5559b78dae4d41660baa17e3cef52472b069c638
Richard Le <169632088+rlechuong@users.noreply.github.com>
2025-04-27 19:17:41 -0400

    Refactor to use class constructor and factories

--- scripts.js
+++ scripts.js
@@ -1,163 +1,175 @@
-const myLibrary = [];
-const bookshelfBody = document.querySelector("#bookshelf-body");
-const dialog = document.querySelector("dialog");
-const addBookButton = document.querySelector("#add-book-button");
-const closeFormButton = document.querySelector("#close-form-button");
-const submitBookButton = document.querySelector("#submit-book-button");
-
-function Book(id, author, title, pages, read) {
-  if (!new.target) {
-    throw Error("You must use the 'new' operator to call the constructor.");
+const Book = class {
+  constructor(id, author, title, pages, read) {
+    this.id = id;
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.read = read;
   }
-  this.id = id;
-  this.author = author;
-  this.title = title;
-  this.pages = pages;
-  this.read = read;
-}
 
-Book.prototype.changeRead = function () {
-  if (this.read === true) {
-    this.read = false;
-  } else {
-    this.read = true;
+  changeRead() {
+    if (this.read === true) {
+      this.read = false;
+    } else {
+      this.read = true;
+    }
   }
 };
 
-function addBookToLibrary(author, title, pages, read) {
-  let newID = crypto.randomUUID();
+const Library = (function () {
+  const books = [];
 
-  let newBook = new Book(newID, author, title, pages, read);
+  const addBook = function (author, title, pages, read) {
+    let newID = crypto.randomUUID();
 
-  myLibrary.push(newBook);
-}
+    let newBook = new Book(newID, author, title, pages, read);
 
-function displayBooks() {
-  for (const book of myLibrary) {
-    const newRow = document.createElement("tr");
+    books.push(newBook);
+  };
 
-    for (let key in book) {
-      if (
-        key === "author" ||
-        key === "title" ||
-        key === "pages" ||
-        key === "read"
-      ) {
-        const newCell = document.createElement("td");
-        newCell.setAttribute("data-id", book["id"]);
-        newCell.textContent = book[key];
-        newRow.appendChild(newCell);
+  const getBooks = function () {
+    return books;
+  };
+
+  return { addBook, getBooks };
+})();
+
+const displayController = (function () {
+  const bookshelfBody = document.querySelector("#bookshelf-body");
+  const dialog = document.querySelector("dialog");
+  const submitBookButton = document.querySelector("#submit-book-button");
+  const closeFormButton = document.querySelector("#close-form-button");
+  const addBookButton = document.querySelector("#add-book-button");
+
+  const displayBooks = () => {
+    for (const book of Library.getBooks()) {
+      const newRow = document.createElement("tr");
+
+      for (let key in book) {
+        if (
+          key === "author" ||
+          key === "title" ||
+          key === "pages" ||
+          key === "read"
+        ) {
+          const newCell = document.createElement("td");
+          newCell.setAttribute("data-id", book["id"]);
+          newCell.textContent = book[key];
+          newRow.appendChild(newCell);
+        }
       }
+
+      const readButton = document.createElement("button");
+      readButton.setAttribute("data-id", book["id"]);
+      readButton.setAttribute("class", "read-button");
+      if (book["read"] === true) {
+        readButton.textContent = "Unread";
+      } else if (book["read"] === false) {
+        readButton.textContent = "Read";
+      }
+      newRow.appendChild(readButton);
+
+      const removeButton = document.createElement("button");
+      removeButton.setAttribute("data-id", book["id"]);
+      removeButton.setAttribute("class", "remove-button");
+      removeButton.textContent = "X";
+      newRow.appendChild(removeButton);
+
+      bookshelfBody.appendChild(newRow);
     }
 
-    const readButton = document.createElement("button");
-    readButton.setAttribute("data-id", book["id"]);
-    readButton.setAttribute("class", "read-button");
-    if (book["read"] === true) {
-      readButton.textContent = "Unread";
-    } else if (book["read"] === false) {
-      readButton.textContent = "Read";
+    readButtonEvents();
+    removeButtonEvents();
+  };
+
+  submitBookButton.addEventListener("click", (event) => {
+    const form = document.querySelector("form");
+
+    if (form.reportValidity()) {
+      event.preventDefault();
+
+      let formAuthorInput = document.querySelector("#author");
+      let formTitleInput = document.querySelector("#title");
+      let formPagesInput = document.querySelector("#pages");
+      let formReadInput = document.querySelector("#read");
+
+      Library.addBook(
+        formAuthorInput.value,
+        formTitleInput.value,
+        formPagesInput.value,
+        formReadInput.checked
+      );
+
+      formAuthorInput.value = "";
+      formTitleInput.value = "";
+      formPagesInput.value = "";
+      formReadInput.checked = false;
+
+      reloadTable();
+
+      dialog.close();
+    } else {
     }
-    newRow.appendChild(readButton);
+  });
 
-    const removeButton = document.createElement("button");
-    removeButton.setAttribute("data-id", book["id"]);
-    removeButton.setAttribute("class", "remove-button");
-    removeButton.textContent = "X";
-    newRow.appendChild(removeButton);
+  const readButtonEvents = function () {
+    const readButtonList = document.querySelectorAll(".read-button");
 
-    bookshelfBody.appendChild(newRow);
-  }
-
-  readButtonEvents();
-  removeButtonEvents();
-}
-
-addBookButton.addEventListener("click", () => {
-  dialog.showModal();
-});
-
-closeFormButton.addEventListener("click", () => {
-  dialog.close();
-});
-
-submitBookButton.addEventListener("click", (event) => {
-  let form = document.querySelector("form");
-
-  if (form.checkValidity()) {
-    event.preventDefault();
-
-    let formAuthor = document.querySelector("#author");
-    let formTitle = document.querySelector("#title");
-    let formPages = document.querySelector("#pages");
-    let formRead = document.querySelector("#read");
-
-    addBookToLibrary(
-      formAuthor.value,
-      formTitle.value,
-      formPages.value,
-      formRead.checked
-    );
-
-    formAuthor.value = "";
-    formTitle.value = "";
-    formPages.value = "";
-    formRead.checked = false;
-
-    reloadTable();
-
-    dialog.close();
-  } else {
-  }
-});
-
-function removeButtonEvents() {
-  const removeButtonList = document.querySelectorAll(".remove-button");
-
-  removeButtonList.forEach(function (button) {
-    button.addEventListener("click", () => {
-      if (confirm("Remove Book Fo Sho?")) {
+    readButtonList.forEach(function (button) {
+      button.addEventListener("click", () => {
         let bookID = button.getAttribute("data-id");
 
-        let targetBook = myLibrary.filter(function (book) {
+        let targetBookList = Library.getBooks().filter(function (book) {
           return book.id === bookID;
         });
 
-        let indexOfTargetBook = myLibrary.indexOf(targetBook[0]);
-        myLibrary.splice(indexOfTargetBook, 1);
+        targetBookList[0].changeRead();
 
         reloadTable();
-      }
-    });
-  });
-}
-
-function readButtonEvents() {
-  const readButtonList = document.querySelectorAll(".read-button");
-
-  readButtonList.forEach(function (button) {
-    button.addEventListener("click", () => {
-      let bookID = button.getAttribute("data-id");
-
-      let targetBook = myLibrary.filter(function (book) {
-        return book.id === bookID;
       });
-
-      targetBook[0].changeRead();
-
-      reloadTable();
     });
+  };
+
+  const removeButtonEvents = function () {
+    const removeButtonList = document.querySelectorAll(".remove-button");
+
+    removeButtonList.forEach(function (button) {
+      button.addEventListener("click", () => {
+        if (confirm("Remove Book Fo Sho?")) {
+          let bookID = button.getAttribute("data-id");
+
+          let targetBookList = Library.getBooks().filter(function (book) {
+            return book.id === bookID;
+          });
+
+          let indexOfTargetBook = Library.getBooks().indexOf(targetBookList[0]);
+          Library.getBooks().splice(indexOfTargetBook, 1);
+
+          reloadTable();
+        }
+      });
+    });
+  };
+
+  const reloadTable = function () {
+    bookshelfBody.textContent = "";
+    displayBooks();
+  };
+
+  closeFormButton.addEventListener("click", () => {
+    dialog.close();
   });
-}
 
-function reloadTable() {
-  bookshelfBody.textContent = "";
-  displayBooks();
-}
+  addBookButton.addEventListener("click", () => {
+    dialog.showModal();
+  });
 
-addBookToLibrary("Author 1", "Title 1", 1000, true);
-addBookToLibrary("Author 2", "Title 2", 2000, false);
-addBookToLibrary("Author 3", "Title 3", 3000, true);
-addBookToLibrary("Author 4", "Title 4", 4000, false);
+  return { displayBooks };
+})();
 
-displayBooks();
+Library.addBook("Author 1", "Title 1", 1000, true);
+Library.addBook("Author 2", "Title 2", 2000, false);
+Library.addBook("Author 3", "Title 3", 3000, true);
+Library.addBook("Author 4", "Title 4", 4000, false);
+
+displayController.displayBooks();
